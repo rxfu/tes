60c501c3ae26e1b8ffe9cd4b30eca15bdc9cef1c
Stephen Fu <stephen.frx@gmail.com>
2019-02-13 14:35:43 +0800

    modify user model

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,12 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->increments('id')->comment('用户ID');
+            $table->string('username', 128)->unique()->comment('用户名');
+            $table->string('password')->comment('密码');
+            $table->string('realname', 50)->comment('真实姓名');
+            $table->string('email')->unique()->nullable()->comment('Email');
+            $table->string('telephone', 20)->nullable()->comment('联系电话');
             $table->rememberToken();
             $table->timestamps();
         });
