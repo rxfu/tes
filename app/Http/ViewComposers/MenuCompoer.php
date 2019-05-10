f2601f6c2df8406d214df83b21320473ee81a94c
Stephen Fu <stephen.frx@gmail.com>
2019-05-10 14:52:41 +0800

    update menu

--- app/Http/ViewComposers/MenuCompoer.php
+++ app/Http/ViewComposers/MenuCompoer.php
@@ -16,7 +16,22 @@ class MenuComposer
      */
     public function compose(View $view)
     {
-        $title = Arr::get(config('menu.sidebar'), Route::currentRouteName() . '.title');
+        $module = request()->segment(1);
+        $sidebar = config('menu.sidebar');
+
+        if (Arr::has($sidebar, $module . '.title')) {
+            $title = Arr::get($sidebar, $module . '.title');
+        } else {
+            foreach ($sidebar as $item) {
+                if (is_array($item) && array_key_exists('children', $item)) {
+                    if (is_null($title = Arr::get($item['children'], $module . '.title'))) {
+                        continue;
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
 
         $view->with(compact('title'));
     }
